2a47302ec27cba2a7dcb614b044bec47c6f81cd5
SgtANKT <ankitpratihast11@gmail.com>
2021-04-20 15:30:18 +0530

    new project

---
+++ src/Robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Ankit Pratihast',
+        username: 'Ankit',
+        email: 'ankit@pratihast.com'
+    },
+    {
+        id: 2,
+        name: 'Ankit Rajput',
+        username: 'Rajput',
+        email: 'ankit@rajput.com'
+    },
+    {
+        id: 3,
+        name: 'Ankit Tiwari',
+        username: 'Tiwari',
+        email: 'ankit@tiwari.com'
+    },
+    {
+        id: 4,
+        name: 'Ankit Sharma',
+        username: 'Sharma',
+        email: 'ankit@sharma.com'
+    },
+    {
+        id: 5,
+        name: 'Ankit Jha',
+        username: 'jha',
+        email: 'ankit@jha.com'
+    },
+    {
+        id: 6,
+        name: 'Ankit Chavan',
+        username: 'Chavan',
+        email: 'ankit@chavan.com'
+    },
+    {
+        id: 7,
+        name: 'Ankit Pandit',
+        username: 'Pandit',
+        email: 'ankit@pandit.com'
+    },
+    {
+        id: 8,
+        name: 'Ankit Singh',
+        username: 'Singh',
+        email: 'ankit@singh.com'
+    },
+    {
+        id: 9,
+        name: 'Ankit Yolo',
+        username: 'Yolo',
+        email: 'ankit@yolo.com'
+    },
+    {
+        id: 10,
+        name: 'Ankit Lal',
+        username: 'Lal',
+        email: 'ankit@lal.com'
+    },
+    {
+        id: 11,
+        name: 'Ankit Dube',
+        username: 'Dube',
+        email: 'ankit@dube.com'
+    },
+    {
+        id: 12,
+        name: 'Ankit Tomar',
+        username: 'Tomar',
+        email: 'ankit@tomar.com'
+    }
+]
